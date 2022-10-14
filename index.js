75786a436a5ec91ff846ace7e48d099715d7e4b8
Bogdan Marcu <bmarcu@yonder.local>
2022-10-14 14:31:09 +0300

    vineri amiaza

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
-const bcrypt = require('bcrypt')
 const db = require('./db')
 
-const saltRounds = 10;
 
 function getReq(req,res,next){
 
