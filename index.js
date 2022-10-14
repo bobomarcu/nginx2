f5859f4a106ddd0e6538074714d079312f6d6c24
Bogdan Marcu <bmarcu@yonder.local>
2022-10-14 14:49:52 +0300

    vineri amiaza

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.get('/login/:username-:password',(req,res)=>{
 
             if(result.length > 0 ){
 
-                if (result.password == password){
+                if (result[0].password == password){
 
                         console.log({id: result.id , username: result.username})
                         res.send({id: result.id , username: result.username})
