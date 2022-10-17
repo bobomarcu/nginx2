4f80f36ab7d542c2ad7a96f2cf16313c38de370a
Bogdan Marcu <bmarcu@yonder.local>
2022-10-17 09:15:28 +0300

    luni dimi

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql')
 var conn  = mysql.createConnection({
 
 
-    host:'192.168.100.53',
+    host:'192.168.7.156',
     port:'3306',
     user:'bogdan',
     password:'root',
